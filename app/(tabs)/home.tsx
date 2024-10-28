fcd46901364fccc03bb2689114b09b99e6c1be6e
Shireesh Marla <shireesh18@gmail.com>
2024-10-28 18:02:25 +0000

    animation added to the video cards

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,26 +1,35 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React, { useState } from 'react';
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 import SearchInput from '@/components/SearchInput';
 import Trending from '@/components/Trending';
 import EmptyState from '@/components/EmptyState';
+import { getAllPosts, getLatestPosts } from '@/lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
+import VideoCard from '@/components/VideoCard';
 
 const Home = () => {
+
+  const { data: posts, refetch, } = useAppwrite(getAllPosts);
+
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
     setRefreshing(true);
-    // re call videos -> if any new videos appear 
+    await refetch();
     setRefreshing(false);
   }
+
   return (
     <SafeAreaView className='bg-primary h-full'>
       <FlatList 
-      data={[{ id: 42 }]}
+      data={ posts }
       keyExtractor={(item) => item.$id }
       renderItem={ ({ item }) => (
-        <Text className='text-3xl text-white'>{item.id}</Text>
+        <VideoCard  video = {item}/>
       )}
       ListHeaderComponent={() => (
         <View className='my-6 px-4 space-y-6'>
@@ -46,7 +55,7 @@ const Home = () => {
               Latest Videos
             </Text>
 
-            <Trending posts={[ {id: 1}, {id:2}, {id:3} ] ?? [] }/>
+            <Trending posts={ latestPosts ?? [] }/>
 
           </View>
         </View>
